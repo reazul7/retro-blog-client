0216d256ca828f2fda35ad22e013c1be9164046e
reazul7 <islamreazul7777@gmail.com>
2021-06-24 12:03:30 +0600

    add navbar using tailwind

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './Banner';
+import NavBar from './NavBar';
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar/>
+            <Banner/>
+        </div>
+    );
+};
+
+export default Home;
